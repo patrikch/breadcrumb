202c7fac188af56d24e7c9ac24d39564afad5bad
Patrik Chytil <patrikch@centrum.cz>
2014-12-09 17:01:18 +0100

    add js and html test files

--- breadcrumbWeb/breadcrumbWeb/settings.py
+++ breadcrumbWeb/breadcrumbWeb/settings.py
@@ -84,4 +84,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/site_media/'
+
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'site_media'),
+)
--- breadcrumbWeb/breadcrumbWeb/urls.py
+++ breadcrumbWeb/breadcrumbWeb/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import include, url
-
 from rest_framework.authtoken.views import obtain_auth_token
-
 from breadcrumbApp.urls import router
+import os
+
+site_media = os.path.join(os.path.dirname(__file__),
+    "site_media")
 
 
 urlpatterns = [    
     #url(r'^api/token/', obtain_auth_token,name="api-token"),
-    url(r"^api/",include(router.urls)), 
+    url(r"^api/",include(router.urls)),
+    url(r"^site_media/(?P<path>.*)$","django.views.static.serve",
+         {"document_root":site_media}),
 ]
